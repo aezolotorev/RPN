56d5964199d99aa856f0bfaa03751412cfc32a30
aezolotorev <aezolotorev@mail.ru>
2020-10-22 15:14:12 +0300

    Ready
    1 commit

---
+++ CalculateRPN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateRPN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("введите выражение: ");
+                try
+                {
+                    Console.WriteLine(RNP.Calculate(Console.ReadLine()));
+                }
+                catch
+                {
+                    Console.WriteLine("Необходимо вводить только числа и арифметические операторы");
+                }
+            }
+        }
+    }
+}
